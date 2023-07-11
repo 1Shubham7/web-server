09d4fed5c19537dc0798b817cd9e276ce670692a
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-11 10:23:42 +0530

    handlers, handlefunc and more

--- main.go
+++ main.go
@@ -8,4 +8,13 @@ import (
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/",fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("starting server at port 8080\n")
+
+	if err := http.ListenAndServe(":8080",nil); err != nil {
+		log.Fatal(err)
+	}
 }
